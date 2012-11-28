5ee1248c2aadaf3cef09e429549cd5c7e8467f6b
Jiek <Jiek@JiekdeMacBook-Pro.local>
2012-11-28 11:26:02 +0800

    Initial Commit

---
+++ 万网（符捷凯）/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  万网（符捷凯）
+//
+//  Created by Jiek on 12-11-28.
+//  Copyright (c) 2012年 Jiek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 万网（符捷凯）/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  万网（符捷凯）
+//
+//  Created by Jiek on 12-11-28.
+//  Copyright (c) 2012年 Jiek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
